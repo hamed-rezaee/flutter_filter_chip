b6422b8fb8bcda3166ddf4b993a5b7364d47d3c1
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-04-18 16:25:58 +0800

    add filter chip logic

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,9 +10,85 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<Category> _selectedCategory = <Category>[];
+
+  final List<Item> _items = <Item>[
+    Item('Cup', [Category.lowPrice, Category.highRate]),
+    Item('Coffee', [Category.lowPrice, Category.lowRate]),
+    Item('Tea', [Category.lowPrice]),
+    Item('Table', [Category.highPrice, Category.highRate]),
+    Item('Chair', [Category.highPrice]),
+  ];
+
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: Text(widget.title)),
-        body: const Center(),
-      );
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Column(
+        children: <Widget>[
+          Wrap(
+            spacing: 8,
+            children: Category.values
+                .map(
+                  (Category category) => FilterChip(
+                    elevation: 2,
+                    label: Text(
+                      category.name,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    selected: _selectedCategory.contains(category),
+                    selectedColor: Colors.blue,
+                    checkmarkColor: Colors.white,
+                    onSelected: (bool selected) {
+                      selected
+                          ? _selectedCategory.add(category)
+                          : _selectedCategory.remove(category);
+
+                      setState(() {});
+                    },
+                  ),
+                )
+                .toList(),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _getItems(_items).length,
+              itemBuilder: (BuildContext context, int index) {
+                final Item item = _getItems(_items)[index];
+
+                return ListTile(
+                  title: Text(item.name),
+                  subtitle: Row(
+                    children: item.categories
+                        .map((element) =>
+                            Text('| ${element.name.toUpperCase()} |'))
+                        .toList(),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<Item> _getItems(List<Item> items) => items
+      .where((item) => _selectedCategory
+          .any((category) => item.categories.contains(category)))
+      .toList();
+}
+
+class Item {
+  Item(this.name, this.categories);
+
+  final String name;
+  final List<Category> categories;
+}
+
+enum Category {
+  highPrice,
+  lowPrice,
+  highRate,
+  lowRate,
 }
